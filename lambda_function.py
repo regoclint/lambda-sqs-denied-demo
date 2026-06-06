c9ba97c707ebc5bf1104356a85eba12ceea1c97c
regoclint <regoclint@gmail.com>
2026-06-06 12:40:18 -0700

    Read queue URL from env var injected by CloudFormation

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,6 @@
-import boto3
+import boto3, os
 
 def handler(event, context):
     sqs = boto3.client("sqs", region_name="us-east-1")
-    queue_url = "https://sqs.us-east-1.amazonaws.com/314820560132/demo-blocked-queue"
-    sqs.send_message(QueueUrl=queue_url, MessageBody="hello from lambda")
+    sqs.send_message(QueueUrl=os.environ["QUEUE_URL"], MessageBody="hello from lambda")
     return {"status": "sent"}
