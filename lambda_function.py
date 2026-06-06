d5af37feebc3d8f56123ce0b459342c4c1f64d1c
regoclint <regoclint@gmail.com>
2026-06-06 12:48:12 -0700

    Add logging to Lambda handler (#1)

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,10 @@
-import boto3, os
+import boto3, os, logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def handler(event, context):
+    logger.info("Attempting to send message to SQS queue: %s", os.environ["QUEUE_URL"])
     sqs = boto3.client("sqs", region_name="us-east-1")
     sqs.send_message(QueueUrl=os.environ["QUEUE_URL"], MessageBody="hello from lambda")
     return {"status": "sent"}
